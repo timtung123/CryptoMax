cafe0e540fdecfa4c2b0edcaf0ff2f0b080dd484
timtung123 <108850507+timtung123@users.noreply.github.com>
2022-07-07 19:21:42 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -162,8 +162,8 @@ function App() {
 
   const incrementMintAmount = () => {
     let newMintAmount = mintAmount + 1;
-    if (newMintAmount > 50) {
-      newMintAmount = 50;
+    if (newMintAmount > 5) {
+      newMintAmount = 5;
     }
     setMintAmount(newMintAmount);
   };
@@ -355,7 +355,7 @@ function App() {
                           color: "var(--accent-text)",
                         }}
                       >
-                        {mintAmount=1}
+                        mintAmount
                       </s.TextDescription>
                       <s.SpacerMedium />
                       <StyledRoundButton
