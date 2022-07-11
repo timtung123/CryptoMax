ba7c35944a6164c0143e491a56e3875716958a30
timtung123 <108850507+timtung123@users.noreply.github.com>
2022-07-11 17:19:33 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -131,7 +131,7 @@ function App() {
     setClaimingNft(true);
     console.log(blockchain)
     blockchain.smartContract.methods
-      .mint(count)
+      .mint(mintAmount)
       .send({
         gasLimit: String(totalGasLimit),
         to: CONFIG.CONTRACT_ADDRESS,
