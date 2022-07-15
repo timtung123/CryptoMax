5d43617d20bb4b51cde197743bc8e9f274f1a2a6
timtung123 <108850507+timtung123@users.noreply.github.com>
2022-07-15 14:45:55 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -135,7 +135,7 @@ function App() {
     let cost = CONFIG.WEI_COST;
     let gasLimit = CONFIG.GAS_LIMIT;
     let totalCostWei = String(cost * mintAmount);
-    let totalGasLimit = String(gasLimit * mintAmount);
+    let totalGasLimit = String(gasLimit);
     console.log("Cost: ", totalCostWei);
     console.log("Gas limit: ", totalGasLimit);
     setFeedback(`Minting your ${CONFIG.NFT_NAME}...`);
